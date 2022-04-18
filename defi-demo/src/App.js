84db9dc399b5ce9e68c79a4919a23bf39fbe5b37
BlackChar92 <Kim.xl@protonmail.com>
2022-04-18 16:49:32 +0800

    perform demo

--- defi-demo/src/App.js
+++ defi-demo/src/App.js
@@ -1,14 +1,15 @@
+import React from 'react';
 import { Link } from 'react-router-dom';
 import './App.scss';
 
 function App() {
-return (
+  return (
     <div className="App">
       <section className="menu">
         <h3>Menu: </h3>
         <Link to="./connector">Connector Demo</Link>
         <Link to="./contract">Contract-Interact Demo</Link>
-        <Link to="./transaction">Transaction-Confirm Demo</Link>
+        <Link to="./confirm">Transaction-Confirm Demo</Link>
         <Link to="./tokenlist">Token-List Demo</Link>
       </section>
     </div>
--- defi-demo/src/index.js
+++ defi-demo/src/index.js
@@ -1,10 +1,11 @@
+import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 import Connector from './routes/connector';
 import Contract from './routes/contract';
 import Confirm from './routes/confirm';
-import Transaction from './routes/transaction';
+// import Transaction from './routes/transaction';
 import TokenList from './routes/tokenList';
 import './index.css';
 
@@ -15,8 +16,8 @@ render(
       <Route path="/" element={<App />} />
       <Route path="connector" element={<Connector />} />
       <Route path="contract" element={<Contract />} />
-        <Route path="confirm" element={<Confirm />} />
-      <Route path="transaction" element={<Transaction />} />
+      <Route path="confirm" element={<Confirm />} />
+      {/* <Route path="transaction" element={<Transaction />} /> */}
       <Route path="tokenlist" element={<TokenList />} />
     </Routes>
   </BrowserRouter>,
