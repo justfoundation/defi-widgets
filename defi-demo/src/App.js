651bb7f883d13c41e1d83bb9bdb39615a2034273
tysonzone <tron_test@protonmail.com>
2022-04-18 15:39:32 +0800

    add transaction-confirm demo

--- defi-demo/src/App.js
+++ defi-demo/src/App.js
@@ -2,15 +2,16 @@ import { Link } from 'react-router-dom';
 import './App.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <section className="menu">
-        <h3>Menu: </h3>
-        <Link to="./connector">Connector Demo</Link>
-        <Link to="./contract">Contract-Interact Demo</Link>
-      </section>
-    </div>
-  );
+    return (
+        <div className="App">
+            <section className="menu">
+                <h3>Menu: </h3>
+                <Link to="./connector">Connector Demo</Link>
+                <Link to="./contract">Contract-Interact Demo</Link>
+                <Link to="./confirm">Transaction-Confirm Demo</Link>
+            </section>
+        </div>
+    );
 }
 
 export default App;
--- defi-demo/src/index.js
+++ defi-demo/src/index.js
@@ -1,22 +1,20 @@
 import { render } from 'react-dom';
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 import Connector from './routes/connector';
 import Contract from './routes/contract';
+import Confirm from './routes/confirm';
 import './index.css';
 
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById('root');
 render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="connector" element={<Connector />} />
-      <Route path="contract" element={<Contract />} />
-    </Routes>
-  </BrowserRouter>,
-  rootElement
-)
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="connector" element={<Connector />} />
+            <Route path="contract" element={<Contract />} />
+            <Route path="confirm" element={<Confirm />} />
+        </Routes>
+    </BrowserRouter>,
+    rootElement
+);
