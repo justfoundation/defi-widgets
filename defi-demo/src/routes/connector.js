94b57d4a609ae31146915b76ce7e5c620bd10c59
BlackChar92 <Kim.xl@protonmail.com>
2022-04-21 22:01:32 +0800

    add spin for connector demo

--- defi-demo/src/routes/connector.js
+++ defi-demo/src/routes/connector.js
@@ -2,12 +2,14 @@ import React, { useState, useEffect } from 'react';
 import BigNumber from 'bignumber.js';
 import { TronWebConnector } from '@widgets/tronweb-connector';
 import Menu from '../components/menu';
+import { Spin } from 'antd';
 
 function App() {
   const [defaultAccount, setDefaultAccount] = useState('');
   const [defaultAccountBalance, setDefaultAccountBalance] = useState('--');
   const [accountsChangedMsg, setAccountsChangedMsg] = useState('');
-  
+  const [loading, setLoading] = useState(false);
+
   const trxPrecision = 1e6;
 
   useEffect(() => {
@@ -25,17 +27,23 @@ function App() {
   };
 
   const activate = async () => {
-    const tronWeb = await TronWebConnector.activate();
-
-    if (tronWeb?.defaultAddress?.base58) {
-      initUserInfo(tronWeb.defaultAddress.base58);
+    setAccountsChangedMsg('');
+    setLoading(true);
+    const res = await TronWebConnector.activate();
+    setLoading(false);
+    if (res?.defaultAddress?.base58) {
+      initUserInfo(res.defaultAddress.base58);
+    } else if (!res?.success && res?.msg) {
+      setAccountsChangedMsg(res.msg);
+    } else {
+      setAccountsChangedMsg(`Please install and log in to tronlink first`);
     }
   };
 
   const addListener = () => {
     TronWebConnector.on('accountsChanged', res => {
       if (res.action === 'accountsChanged')
-      setDefaultAccount(res.data.address);
+        setDefaultAccount(res.data.address);
       setAccountsChangedMsg(`Current account address is: ${res.data.address}`);
     })
   };
@@ -50,7 +58,7 @@ function App() {
               <div><span>Current account: </span>{defaultAccount}</div>
               <div><span>Current account balance: </span>{defaultAccountBalance.toString()} TRX</div>
             </div>
-            
+
             <div className='items'>
               <div className='item' onClick={() => addListener()} style={{ marginTop: '10px' }}>On accountsChanged</div>
             </div>
@@ -61,6 +69,7 @@ function App() {
           </div>
         }
         {accountsChangedMsg && <div className='msg' title={accountsChangedMsg}>Result message: {accountsChangedMsg}</div>}
+        <Spin spinning={loading} />
       </section>
     </div>
   );
