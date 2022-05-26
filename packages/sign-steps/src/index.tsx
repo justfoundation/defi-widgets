20d1b6e68a6a4f812af006b86c03488e0e619ff6
BlackChar92 <Kim.xl@protonmail.com>
2022-05-26 16:07:06 +0800

    sign-steps dev

--- packages/sign-steps/src/index.tsx
+++ packages/sign-steps/src/index.tsx
@@ -1,6 +1,45 @@
-import * as React from 'react';
+'use strict';
 
-// Delete me
-export const Thing = () => {
-  return <div>the snozzberries taste like snozzberries</div>;
-};
+interface ResultType {
+  success: boolean;
+  msg?: string;
+  data?: any;
+}
+
+export class SignSteps {
+  public stepNumber: number = 0;
+  public completeNumber: number = 0;
+
+  private errorMessage = (msg: string) => {
+    const error: ResultType = { success: false, msg };
+    return error;
+  };
+
+  private successData = (data?: any) => {
+    const result: ResultType = { success: true, data };
+    return result;
+  };
+
+  public getStepNumber = () => {
+    return this.stepNumber;
+  };
+
+  private setStepNumber = (stepNumber: number) => {
+    this.stepNumber = stepNumber;
+  }
+
+  public executeSignsSimple = async (functions: Array<Function>, { callbacks = () => {}}) => {
+    this.completeNumber = 0;
+    try {
+      functions.map(async (func) => {
+        await func();
+        this.setStepNumber(++this.completeNumber);
+      })
+      callbacks && callbacks();
+      return this.successData({ completedAmount: this.completeNumber });
+    } catch (error) {
+      this.setStepNumber(this.completeNumber);
+      return this.errorMessage(`error: Continuous execution error, currently executed to the ${this.completeNumber} step, error message: ${error}`);
+    }
+  }
+}
