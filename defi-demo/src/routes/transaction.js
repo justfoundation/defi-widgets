3318d6802fdf755e9a52b9828a093bc6f376b77d
hectorchanht <hectorchanht@gmail.com>
2022-06-06 16:05:32 +0800

    added Contract execution flow & <Steps>

--- defi-demo/src/routes/transaction.js
+++ defi-demo/src/routes/transaction.js
@@ -1,23 +1,17 @@
-import { useState, useEffect } from 'react';
-import '../App.scss';
-import BigNumber from 'bignumber.js';
-import { TronWebConnector } from '@widgets/tronweb-connector';
 import { ContractInteract } from '@widgets/contract-interact';
-import {
-  OpenTransModal,
-  setTransactionsData,
-  getTransactionInfo,
-  getDescription,
-  checkPendingTransactions,
-  logTransaction,
-  saveTransactions,
-  setVariablesInterval
-} from '@widgets/transaction-confirm';
+import { OpenTransModal } from '@widgets/transaction-confirm';
+import { TronWebConnector } from '@widgets/tronweb-connector';
+import { Steps } from 'antd';
+import BigNumber from 'bignumber.js';
+import { useEffect, useState } from 'react';
+import '../App.scss';
 import Menu from '../components/menu';
 const { trigger, sign, broadcast, send, sendTrx } = ContractInteract;
+const { Step } = Steps;
 
 function App() {
   const [defaultAccount, setDefaultAccount] = useState('');
+  const [sendTrxStep, setSendTrxStep] = useState(0);
   const [defaultAccountBalance, setDefaultAccountBalance] = useState('--');
 
   const trxPrecision = 1e6;
@@ -33,7 +27,11 @@ function App() {
     const accountInfo = await window.tronWeb.trx.getAccount(userAddress);
     const accountBalance = new BigNumber(accountInfo.balance).div(trxPrecision);
     setDefaultAccountBalance(accountBalance);
+    nextStep();
   };
+
+  const nextStep = () => setSendTrxStep(s => s + 1);
+  const backStep = (step = 1) => setSendTrxStep(step); // todo: back step if fail
 
   const activate = async () => {
     const res = await TronWebConnector.activate();
@@ -43,17 +41,26 @@ function App() {
   }
 
   const sendTrxFunc = async () => {
-    OpenTransModal({step: 1});
-    const res = await sendTrx(
-      'TBHHa5Z6WQ1cRcgUhdvqdW4f728f2fiJmF',
-      1000000
-    );
+    try {
+      OpenTransModal({ step: 1 });
+      nextStep();
+    
+      const res = await sendTrx(
+        'TBHHa5Z6WQ1cRcgUhdvqdW4f728f2fiJmF',
+        1000000
+      );
 
-    if (res?.result) {
-      console.log(res); // txid
-      OpenTransModal({step: 2 }, {title: 'Send TRX success'});
-    } else {
-      OpenTransModal({step: 3}, {title: 'Send TRX failed'});
+      if (res?.result) {
+        nextStep();
+        OpenTransModal({ step: 2 }, { title: 'Send TRX success' });
+      } else {
+        backStep();
+        OpenTransModal({ step: 3 }, { title: 'Send TRX failed' });
+      }
+    }
+    catch (e) {
+      OpenTransModal({ step: 3 }, { title: 'Send TRX failed due to ' + e.message });
+      backStep();
     }
   }
 
@@ -76,6 +83,17 @@ function App() {
             <div className='item' onClick={() => activate()}>Connect Wallet</div>
           </div>
         }
+        <br />
+        
+        Contract execution flow
+
+        <Steps style={{paddingLeft: '30%', backgroundColor: 'wheat'}} direction="vertical" current={sendTrxStep}>
+          <Step title="Connection" description="Connect the dapp with your wallet" />
+          <Step title="Initiation" description="Initiate the contract function by pressing 'Send TRX'" />
+          <Step title="Signing" description="Sign the function that you would like to execute" />
+          <Step title="Broadcast" description="Wait for the signed transaction being broadcast to all nodes" />
+          {/* <Step title="On-chain" description="The transaction is on-chain now, Congrats" /> */}
+        </Steps>
       </section>
     </div>
   );
